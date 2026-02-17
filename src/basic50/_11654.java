d612fdb2b9f861a2b8ac96a14722b4947b8ddc21
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2026-02-17 23:56:19 +0900

    11654: 아스키코드

---
+++ src/basic50/_11654.java
@@ -0,0 +1,14 @@
+package basic50;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+
+        System.out.print((int) st.charAt(0));
+    }
+}
